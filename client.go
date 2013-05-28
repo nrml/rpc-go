18020cc2fd8ace93345d9ff05b363976eb369192
Brent Anderson <brentmn@gmail.com>
2013-05-28 11:31:35 -0500

    simplified client code

--- client.go
+++ client.go
@@ -15,12 +15,21 @@ type client struct {
 	service   string
 	rpc       *rpc.Client
 }
+type status struct {
+	Status    int8
+	Namespace string
+}
+type callargs struct {
+	endpoint string
+	msg      Message
+	reply    interface{}
+}
 
 func NewClient(key string, namespace string, svcname string) (client, error) {
 	var err error
 	c := client{}
 	if key == "" || namespace == "" {
-		err = errors.New("needs secret and namespace")
+		err = errors.New("needs key and namespace")
 	}
 	c.service = svcname
 	c.key = key
@@ -39,29 +48,34 @@ func (c *client) Connect(host string, port int64) error {
 	rpcCodec := msgpack.NewCustomRPCClientCodec(conn, nil)
 	client := rpc.NewClientWithCodec(rpcCodec)
 	c.rpc = client
+
+	//call service init
+	st := new(status)
+	err = c.Call("Init", c.key, c.namespace, st)
+
 	return err
 }
 
 //last agrument is the reply that is a zeroed T value pointer (new), all others sent with the request
 func (c *client) Call(method string, args ...interface{}) error {
-	ep := c.endpoint()
-	l := len(args)
-	reqargs := args[0 : l-1]
-	reply := args[l-1:][0]
-	msg := Message{c.key, c.namespace, method, reqargs}
-	var err error
-	err = c.rpc.Call(ep, msg, reply)
-	return err
+	ca := c.build(method, args...)
+	return c.rpc.Call(ca.endpoint, ca.msg, ca.reply)
 }
 
 func (c *client) Async(method string, args ...interface{}) *rpc.Call {
-	ep := c.endpoint()
+	ca := c.build(method, args...)
+	return c.rpc.Go(ca.endpoint, ca.msg, ca.reply, nil)
+}
+func (c *client) build(method string, args ...interface{}) callargs {
+	ca := callargs{}
+	ca.endpoint = c.endpoint()
 	l := len(args)
 	reqargs := args[0 : l-1]
 	reply := args[l-1:][0]
 	msg := Message{c.key, c.namespace, method, reqargs}
-
-	return c.rpc.Go(ep, msg, reply, nil)
+	ca.msg = msg
+	ca.reply = reply
+	return ca
 }
 func (c *client) endpoint() string {
 	return fmt.Sprintf("%sRpc.Call", c.service)
